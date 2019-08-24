069c2a4978172ccc9dc8764c8c8e9141acd15c6e
garciart <rgarcia@rgprogramming.com>
2019-08-23 21:40:42 -0400

    Reviewed code and renamed solution.

---
+++ REALIDCardMaker/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using REALIDCardMaker.Areas.Identity.Data;
+using REALIDCardMaker.Models;
+
+[assembly: HostingStartup(typeof(REALIDCardMaker.Areas.Identity.IdentityHostingStartup))]
+namespace REALIDCardMaker.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<REALIDCardMakerContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("REALIDCardMakerContextConnection")));
+
+                services.AddDefaultIdentity<REALIDCardMakerUser>()
+                    .AddEntityFrameworkStores<REALIDCardMakerContext>();
+            });
+        }
+    }
+}
--- REAL_ID_Card_Maker/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace REAL_ID_Card_Maker.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
