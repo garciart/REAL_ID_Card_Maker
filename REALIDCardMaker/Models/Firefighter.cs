3af6c5de225a814bcb521e087d31da1d31aa35b9
garciart <rgarcia@rgprogramming.com>
2019-08-24 12:28:31 -0400

    Added Firefighter class to check github link.

---
+++ REALIDCardMaker/Models/Firefighter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REALIDCardMaker.Models {
+    public class Firefighter {
+        public int FireFighterID { get; set; }
+        public string FireFighterFName { get; set; }
+        public string FireFighterLName { get; set; }
+        public string FireFighterMI { get; set; }
+        public int FireFighterDOB { get; set; }
+        public int FireFighterSex { get; set; }
+        public int FireFighterHeight { get; set; }
+        public string FireFighterEyeColor { get; set; }
+        public string FireFighterBloodType { get; set; }
+        public int FireFighterStartYear { get; set; }
+        public string FireFighterRank { get; set; }
+        public string FireFighterTitle { get; set; }
+    }
+}
